c3ef12875b8bb6de4f4bab5820358b9ff7556392
hh12356 <hsz2855829417@163.com>
2026-08-27 17:56:15 +0800

    Package and End

--- react-jike/src/router/index.js
+++ react-jike/src/router/index.js
@@ -3,9 +3,12 @@ import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
 import {createBrowserRouter} from 'react-router-dom'
 import {AuthRoute} from '@/components/AuthRoute'
-import Article from '@/pages/Article'
-import Home from '@/pages/Home'
-import Publish from '@/pages/Publish'
+import { lazy, Suspense } from 'react'
+
+//1.lazy函数对组件进行导入
+const Home = lazy(()=>import('@/pages/Home'))
+const Article = lazy(()=>import('@/pages/Article'))
+const Publish = lazy(()=>import('@/pages/Publish'))
 
 //配置路由实例
 const router = createBrowserRouter([
@@ -15,15 +18,16 @@ const router = createBrowserRouter([
         children:[
             {
                 index:true,
-                element:<Home/>
+                element:<Suspense fallback={'加载中'}><Home/></Suspense>
             },
             {
                 path:'article',
-                element:<Article/>
+                element:<Suspense fallback={'加载中'}><Article/></Suspense>
+
             },
             {
                 path:'publish',
-                element:<Publish/>
+                element:<Suspense fallback={'加载中'}><Publish/></Suspense>
             },
         ]
     },
